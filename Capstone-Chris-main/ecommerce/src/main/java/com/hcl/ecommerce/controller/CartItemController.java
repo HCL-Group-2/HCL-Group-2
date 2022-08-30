6aa80a491cae67410904c236b1b887e6f58b23fd
Matt Harvey <torchbird@gmail.com>
2022-08-30 17:15:19 -0500

    updated cartitem

--- Capstone-Chris-main/ecommerce/src/main/java/com/hcl/ecommerce/controller/CartItemController.java
+++ Capstone-Chris-main/ecommerce/src/main/java/com/hcl/ecommerce/controller/CartItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.hcl.ecommerce.dto.CartItemDto;
 import com.hcl.ecommerce.entity.CartItem;
 import com.hcl.ecommerce.service.CartItemService;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class CartItemController {
 	
