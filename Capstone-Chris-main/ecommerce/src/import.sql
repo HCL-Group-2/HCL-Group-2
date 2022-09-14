95a920fb2d10e739190dde1b66a3df80a9696509
AlyseGoodyear <99294125+AlyseGoodyear@users.noreply.github.com>
2022-09-14 08:15:02 -0500

    added sql trigger file

---
+++ Capstone-Chris-main/ecommerce/src/import.sql
@@ -0,0 +1,24 @@
+DROP TRIGGER IF EXISTS `ecommerce`.upd_check;
+
+DELIMITER $$
+CREATE TRIGGER upd_check BEFORE UPDATE ON products
+FOR EACH ROW
+BEGIN 
+	IF NEW.prod_inventory <= 0 THEN
+		SET NEW.prod_inventory =  100 + NEW.prod_inventory;
+	END IF;
+END $$
+
+
+DROP TRIGGER IF EXISTS `ecommerce`.QuantityUpdate;
+
+DELIMITER |
+CREATE TRIGGER  QuantityUpdate
+AFTER INSERT
+ON order_item 
+FOR EACH ROW
+	BEGIN
+		UPDATE products
+		SET products.prod_inventory = products.prod_inventory - New.prod_inventory 
+		WHERE products.id = New.prod_id;
+END |
