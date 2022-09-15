5b2cc577a66772d65f21e110d0f4b960a7cd9faa
Chris Wilcox <chriswilcox580@gmail.com>
2022-09-15 14:00:54 -0500

    Added OAuth2SecurityConfigurerAdapter and CustomAccessDeniedHandler

---
+++ Capstone-Chris-main/ecommerce/src/main/java/com/hcl/ecommerce/controller/CustomAccessDeniedHandler.java
@@ -0,0 +1,18 @@
+package com.hcl.ecommerce.controller;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest req, HttpServletResponse res, AccessDeniedException e) throws IOException {
+        res.sendRedirect("/403");
+    }
+}
