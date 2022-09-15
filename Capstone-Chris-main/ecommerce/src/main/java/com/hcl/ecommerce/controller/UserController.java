ee4b441aec360ca4ee39ab1ec5ea92dcbc1992b6
Chris Wilcox <chriswilcox580@gmail.com>
2022-09-15 14:20:47 -0500

    Update UserController.java

--- Capstone-Chris-main/ecommerce/src/main/java/com/hcl/ecommerce/controller/UserController.java
+++ Capstone-Chris-main/ecommerce/src/main/java/com/hcl/ecommerce/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -65,6 +66,7 @@ public class UserController {
 	}
 	
 	@GetMapping("/users")
+	@PreAuthorize("hasAuthority('admin')")
 	public ResponseEntity<List<User>> getAllUsers() {
 		List<User> list = userService.getAllUsers();
 		return new ResponseEntity<List<User>>(list, HttpStatus.OK);
