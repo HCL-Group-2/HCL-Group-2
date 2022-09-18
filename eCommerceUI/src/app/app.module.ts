09026efba8c2ef0475ba58d5329b9d59c59b0ff1
leePederson <leepederson88@gmail.com>
2022-09-18 11:15:54 -0500

    Update app.module.ts

--- eCommerceUI/src/app/app.module.ts
+++ eCommerceUI/src/app/app.module.ts
@@ -74,7 +74,7 @@ import { EditUserComponent } from './admin/edit-user/edit-user.component';
 
 
 const oktaAuth = new OktaAuth({
-  issuer: 'https://dev-06861319.okta.com/oauth2/default',
+  issuer: "https://dev-34530608.okta.com/oauth2/default",
   clientId: '0oa6b7ee0wwOnJzuz5d7',
   redirectUri: window.location.origin + '/login/callback'
 });
