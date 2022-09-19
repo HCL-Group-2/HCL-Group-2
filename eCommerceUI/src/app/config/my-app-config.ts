ac0432bccfb3e2bcf37594391eaf1e29f588a6c3
leePederson <leepederson88@gmail.com>
2022-09-19 14:56:57 -0500

    Update my-app-config.ts

--- eCommerceUI/src/app/config/my-app-config.ts
+++ eCommerceUI/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
       clientId: "0oa6batm961d386e25d7",
       issuer: "https://dev-34530608.okta.com/oauth2/default",
-      redirectUri: 'http://localhost:4200/login/callback',
+      redirectUri: 'https://ostrichmart.azurewebsites.net/login/callback',
       scopes: ['openid', 'profile', 'email']
     }
   }
