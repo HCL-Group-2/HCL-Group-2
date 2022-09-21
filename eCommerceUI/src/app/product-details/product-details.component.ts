f9528cf653b6241003f11765d73accf285316d6b
leePederson <leepederson88@gmail.com>
2022-09-20 19:33:08 -0500

    Updated product details page to not be hard coded

--- eCommerceUI/src/app/product-details/product-details.component.ts
+++ eCommerceUI/src/app/product-details/product-details.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/model/Product';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../product.service';
+import { FormGroup } from '@angular/forms';
+import { User } from '../model/User';
+import { CartItems } from '../model/CartItems';
+import { CartDialog } from '../home/home.component';
+import { MatDialog } from '@angular/material/dialog';
+import { CartService } from '../cart.service';
 
 
 
@@ -12,7 +19,17 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
 
   product !: Product;
-   constructor(  private router: Router, private route: ActivatedRoute) { 
+  storage: Storage = sessionStorage;
+  cartQuantityForm: FormGroup = new FormGroup([]);
+  user !: User;
+  selectedQuantity: number = 0;
+  selectedProduct !: CartItems;
+  
+   constructor(  private router: Router,
+     private route: ActivatedRoute,
+      private productService: ProductService,
+      private cartService: CartService,
+      public cartDialog: MatDialog) { 
 
      // this.product = this.route.snapshot.params['product'];
      console.log('passing product object test ' +  JSON.stringify(this.router.getCurrentNavigation()?.extras.state));
@@ -21,5 +38,20 @@ export class ProductDetailsComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    this.getProduct();
+    console.log(this.product);
+    
+  
   }
+
+  getProduct(){
+    this.productService.getProductById(Number(this.storage.getItem('productId'))).subscribe(data => {
+      this.product= data;
+      console.log("Occupying product with "+this.storage.getItem('productId'));
+      console.log(this.product);
+      
+    }); 
+  }
+
+  
 }
