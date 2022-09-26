c6746c3157e60825bfa89ad3c74d9a5bcc41caf0
leePederson <leepederson88@gmail.com>
2022-09-26 09:28:45 -0500

    Revert "Merge pull request #94 from HCL-Group-2/75-inventory-management-with-rabbitmq"
    This reverts commit 00389ebcb9c18c3196e61f04302a9df164e7365c, reversing
    changes made to ecd9c591db58dc0e024179dde23fe582bde0736c.

--- Capstone-Chris-main/ecommerce/src/main/java/com/hcl/ecommerce/service/OrderServiceImpl.java
+++ Capstone-Chris-main/ecommerce/src/main/java/com/hcl/ecommerce/service/OrderServiceImpl.java
@@ -19,13 +19,10 @@ import com.hcl.ecommerce.entity.OrderItem;
 import com.hcl.ecommerce.entity.Product;
 import com.hcl.ecommerce.entity.User;
 import com.hcl.ecommerce.exception.AddEntityException;
-import com.hcl.ecommerce.message.MessagingRabbitmqApplication;
 import com.hcl.ecommerce.repository.CartItemRepository;
 import com.hcl.ecommerce.repository.OrderRepository;
 import com.hcl.ecommerce.repository.ProductRepository;
 import com.hcl.ecommerce.repository.UserRepository;
-
-
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -60,9 +57,6 @@ public class OrderServiceImpl implements OrderService {
 		for (CartItem cartItem : cartItems) {
 			Product prod = getProductById(cartItem.getProduct().getId());
 			prod.setInventory(prod.getInventory() - cartItem.getQuantity());
-			if(prod.getInventory() < 100) {
-				System.out.println("Order more " + prod.getName());
-			}
 			productRepository.save(prod);
 			OrderItem orderItem = new OrderItem();
 			BeanUtils.copyProperties(cartItem, orderItem, "id");
