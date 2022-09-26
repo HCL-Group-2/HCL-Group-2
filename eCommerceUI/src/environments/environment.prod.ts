5f4844ff4f112c0c13028baed047faeb7a73b128
leePederson <leepederson88@gmail.com>
2022-09-26 14:51:15 -0500

    added stripe publish key to prod

--- eCommerceUI/src/environments/environment.prod.ts
+++ eCommerceUI/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  stripePublishableKey: "pk_test_51LkxOHHkozRcSFojobOlpb7oOTEwZawfJTlHBMA1g95r44mXzdfp3LOPgz6jKWo90HH68EGST6PGb4uipT77pFt300gtjsMhax"
 };
