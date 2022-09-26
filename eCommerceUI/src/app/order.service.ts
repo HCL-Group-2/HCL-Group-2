78207cc47e2cd6a467c0305fbac324dacb77a87d
leePederson <leepederson88@gmail.com>
2022-09-26 11:42:27 -0500

    Update order.service.ts

--- eCommerceUI/src/app/order.service.ts
+++ eCommerceUI/src/app/order.service.ts
@@ -7,7 +7,7 @@ import { Order } from "./model/Order";
   providedIn: 'root'
 })
 export class OrderService {
-  private baseURL = 'http://ostrichmart-backend.azurewebsites.net/ecommerce/';
+  private baseURL = 'https://ostrichmart-backend.azurewebsites.net/ecommerce/';
 
   constructor(private http: HttpClient) { }
 
