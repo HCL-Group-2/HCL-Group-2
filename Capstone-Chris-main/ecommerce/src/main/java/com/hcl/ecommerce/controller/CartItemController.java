3dfabf4e60a6439fe88ee5a5b65a9881390ecd68
leePederson <78270224+leePederson@users.noreply.github.com>
2022-09-26 13:57:19 -0500

    Update CartItemController.java

--- Capstone-Chris-main/ecommerce/src/main/java/com/hcl/ecommerce/controller/CartItemController.java
+++ Capstone-Chris-main/ecommerce/src/main/java/com/hcl/ecommerce/controller/CartItemController.java
@@ -18,9 +18,7 @@ import com.hcl.ecommerce.entity.CartItem;
 import com.hcl.ecommerce.exception.AddEntityException;
 import com.hcl.ecommerce.service.CartItemService;
 
-// @CrossOrigin(origins="https://ostrichmart.azurewebsites.net")
-
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="https://ostrichmart.azurewebsites.net")
 
 @RestController
 public class CartItemController {
