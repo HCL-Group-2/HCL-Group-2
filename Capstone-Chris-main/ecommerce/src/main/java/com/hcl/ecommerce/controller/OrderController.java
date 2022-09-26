d742e84b761aff9f17717fab23404c4e59f11574
leePederson <leepederson88@gmail.com>
2022-09-26 11:35:27 -0500

    Update OrderController.java

--- Capstone-Chris-main/ecommerce/src/main/java/com/hcl/ecommerce/controller/OrderController.java
+++ Capstone-Chris-main/ecommerce/src/main/java/com/hcl/ecommerce/controller/OrderController.java
@@ -27,7 +27,7 @@ import com.stripe.param.PaymentIntentCreateParams;
 
 
 
-@CrossOrigin
+@CrossOrigin(origins="https://ostrichmart.azurewebsites.net")
 @RestController
 public class OrderController {
 	
