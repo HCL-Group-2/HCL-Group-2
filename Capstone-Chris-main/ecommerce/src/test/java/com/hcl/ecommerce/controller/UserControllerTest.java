5d0c2239e8f4b65c2e439bafd9f1b3d599a46fe7
Flandorfer <cole.flandorfer@gmail.com>
2022-09-30 12:14:17 -0500

    getAllUsers test

--- Capstone-Chris-main/ecommerce/src/test/java/com/hcl/ecommerce/controller/UserControllerTest.java
+++ Capstone-Chris-main/ecommerce/src/test/java/com/hcl/ecommerce/controller/UserControllerTest.java
@@ -3,6 +3,8 @@ package com.hcl.ecommerce.controller;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -21,8 +23,10 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hcl.ecommerce.entity.User;
+import com.hcl.ecommerce.exception.AddEntityException;
 import com.hcl.ecommerce.service.UserService;
 
 @RunWith(SpringRunner.class)
@@ -69,6 +73,13 @@ public class UserControllerTest {
 
 		//Assert that the return status is CREATED
 		assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+		
+		//test that it handles the exception thrown when inserting a duplicate user
+		Mockito.when(userService.addUser(any(User.class))).thenThrow(AddEntityException.class);
+		
+		result = mockMvc.perform(requestBuilder).andReturn();
+		response = result.getResponse();
+		assertEquals(HttpStatus.CONFLICT.value(), response.getStatus());
 
 	}
 
@@ -167,7 +178,42 @@ public class UserControllerTest {
 
 		//Assert that the return status is 204 No Content
 		assertEquals(HttpStatus.NO_CONTENT.value(), response.getStatus());
+	}
+	
+	@Test
+	public void testGetAllUsers() throws Exception{
+		String mockUserListJson = 
+				"["
+					+ "{"
+						+ "\"id\":1,"
+						+ "\"firstName\":\"Test Updated1\","
+						+ "\"lastName\":\"User1\","
+						+ "\"email\":\"testuser1@gmail.com\""
+					+ "},"
+					+ "{"
+						+ "\"id\":2,"
+						+ "\"firstName\":\"Test Updated2\","
+						+ "\"lastName\":\"User2\","
+						+ "\"email\":\"testuser2@gmail.com\""
+					+ "},"
+					+ "{"
+						+ "\"id\":3,"
+						+ "\"firstName\":\"Test Updated3\","
+						+ "\"lastName\":\"User3\","
+						+ "\"email\":\"testuser3@gmail.com\""
+					+ "}"
+				+ "]";
+		
+		ObjectMapper mapper = mapperBuilder.build();
+		List<User> mockUserList = mapper.readValue(mockUserListJson, new TypeReference<List<User>>() {});
+		
+		Mockito.when(userService.getAllUsers()).thenReturn(mockUserList);
+		
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get("/products").accept(MediaType.APPLICATION_JSON);
 
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		assertEquals(mockUserListJson, result.getResponse().getContentAsString());
 	}
 
 }
