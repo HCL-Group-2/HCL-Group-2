4c49a63e734e6c62a1af2614c49864c7e59f871f
Flandorfer <cole.flandorfer@gmail.com>
2022-10-10 16:09:22 -0500

    changing the keys
    not sure how to make the front end connect to local backend

--- eCommerceUI/src/environments/environment.prod.ts
+++ eCommerceUI/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  stripePublishableKey: "pk_test_51LkxOHHkozRcSFojobOlpb7oOTEwZawfJTlHBMA1g95r44mXzdfp3LOPgz6jKWo90HH68EGST6PGb4uipT77pFt300gtjsMhax"
+  stripePublishableKey: "pk_test_51LjknZHoyKsElAxnzL8QVPjidtoicF8IRvKcZupx5xNbQjesDkZRs0gxlmwifiCIFxqqkCmAi7Wwr7Kp0VjL5IpI00hQfhT9Hm"
 };
